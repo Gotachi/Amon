d000ae5811ac50e3f205f93f445ff8c2efb5d49e
Gotachi <33697321+Gotachi@users.noreply.github.com>
2017-11-15 15:08:47 -0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const prefix = "!"
+client.on("ready", () => {
+  console.log(`Em funcionamento!!!! total de ${client.channels.size} Canais, em ${client.guilds.size} servers, um total de ${client.users.size} usuarios.`)
+})
+ 
+ 
+client.login("MzgwMTk3NzQzODE1MTYzOTA0.DO1TyA.sOaVe5tY9tAyAZ--7qi9JfIBDmI")
+ 
+ 
+client.on("message", (message) => {
+  if (message.author.bot) return;
+  if (message.channel.type !== 'text') return;
+  if (!message.content.startsWith("!")) return
+ 
+  if (message.content.startsWith("!" + "regras")) {
+    message.channel.sendMessage("Para ser atendido pelos Oprimions, sua denuncia deverá ter os seguintes itens: Nick, Trapper, Data e Provas. Entendido mortal ?")
+  }
+  if (message.content.startsWith("!" + "ola")) {
+    message.channel.sendMessage("Saudações mortal, sou Amon, o demonio responsável pelas denúncias. Não tenha piedade, vamos punir os culpados.")
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
